0ac660670808f98ac11fdeb96731e58716b52216
Bruniaux <f.bruniaux@gmail.com>
2014-03-06 11:30:56 +0100

    Création module 'menu'
    Module permettant de gérer l'affichage du menu, la surbrillance des
    items etc
    (cf Backbone.picky ...)

---
+++ app/js/common/menu/list/list_controller.js
@@ -0,0 +1,51 @@
+define([
+    'app',
+    'common/menu/list/list_view'
+], function(AppManager, View){
+    
+    AppManager.module('MenuModule.List', function(List, AppManager, Backbone, Marionette, $, _){
+        
+        List.Controller = {
+            
+            listMenu: function(){
+                
+                if(DEBUG) console.info("menu.list.list_controller.listMenu()")
+                
+                require([
+                    'entities/menu'
+                ], function(){
+                    
+                    var links = AppManager.request('menu:entities');
+                    var menu = new View.Menu({collection: links});
+                    
+                    menu.on('logo:clicked', function(){
+                        AppManager.trigger('home:root');
+                    });
+                    
+                    menu.on('itemview:navigate', function(childView, model){
+                        var trigger = model.get('navigationTrigger');
+                        AppManager.trigger(trigger);
+                    });
+                    
+                    AppManager.menuRegion.show(menu);
+                });
+            },
+            
+            setActiveMenuItem: function(menuItemUrl){
+                
+                if(DEBUG) console.info("menu.list.list_controller.setActiveMenuItem()")
+                
+                var links = AppManager.request('menu:entities');
+                
+                var menuItemToSelect = links.find(function(menu){
+                    return menu.get('url') === menuItemUrl;
+                });
+                
+                menuItemToSelect.select();
+                links.trigger('reset');
+            }
+        };
+    });
+
+    return AppManager.MenuModule.List.Controller;
+});
---
+++ app/js/common/menu/list/list_view.js
@@ -0,0 +1,51 @@
+define([
+    'app',
+    'tpl!common/menu/list/templates/list.tpl',
+    'tpl!common/menu/list/templates/list_item.tpl'
+], function(AppManager, ListTpl, ListItemTpl){
+    
+    AppManager.module('MenuModule.List.View', function(View, AppManager, Backbone, Marionette, $, _){
+        
+        View.MenuItems = Marionette.ItemView.extend({
+            
+            template: ListItemTpl,
+            tagName: 'li',
+            
+            events: {
+                'click a': 'navigate'
+            },
+            
+            navigate: function(e){
+                e.preventDefault();
+                this.trigger('navigate', this.model);
+            },
+            
+            onRender: function(){
+                if (this.model.selected) {
+                    //To add class to highlight the active entry in the menu
+                    this.$el.addClass("active");
+                };
+            }
+            
+        });
+        
+        View.Menu = Marionette.CompositeView.extend({
+        
+            template: ListTpl,
+            itemView: View.MenuItems,
+            itemViewContainer: 'ul',
+            
+            events: {
+                'click a#logo': 'logoClicked'
+            },
+
+            logoClicked: function(e){
+                e.preventDefault();
+                this.trigger("logo:clicked");
+            }
+        });
+        
+    });
+
+    return AppManager.MenuModule.List.View;
+})
