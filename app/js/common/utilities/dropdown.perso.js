4375d5db60fe08a86e82628377de3e31d835d902
Bruniaux <f.bruniaux@gmail.com>
2014-03-07 18:39:10 +0100

    Ajout utilities/dropdown
    Permet de gérer les menu déroulant qui apparaissent lors d'un click sur
    les classes .dropdown

---
+++ app/js/common/utilities/dropdown.perso.js
@@ -0,0 +1,28 @@
+define([
+
+],function() {
+    
+    return function(){
+        //===== Add fadeIn animation to dropdown =====//
+
+	$('.dropdown, .btn-group').on('show.bs.dropdown', function(e){
+		$(this).find('.dropdown-menu').first().stop(true, true).fadeIn(100);
+	});
+
+
+	//===== Add fadeOut animation to dropdown =====//
+
+	$('.dropdown, .btn-group').on('hide.bs.dropdown', function(e){
+		$(this).find('.dropdown-menu').first().stop(true, true).fadeOut(100);
+	});
+
+
+	//===== Prevent dropdown from closing on click =====//
+
+	$('.popup').click(function (e) {
+		e.stopPropagation();
+	});
+    }();
+    
+});
+
