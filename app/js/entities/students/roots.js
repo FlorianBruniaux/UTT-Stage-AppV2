4f159a164a5be2e59d4c2ef6015b14572f77fabc
Bruniaux <f.bruniaux@gmail.com>
2014-03-25 09:44:17 +0100

    Creation of 'entities/students/**'

---
+++ app/js/entities/students/roots.js
@@ -0,0 +1,31 @@
+define([
+    'app'
+], function(AppManager){
+    
+    // "ROOTS" ENTITIES
+    AppManager.module('Entities', function(Entities, AppManager, Backbone, Marionette, $, _){
+        
+        // Model
+        Entities.Roots = Backbone.Model.extend({
+        });
+        
+        // Collection
+        Entities.RootsCollection = Backbone.Collection.extend({
+            model : Entities.Roots,
+        });
+        
+        //Home
+        AppManager.reqres.setHandler('students:homeRoot:entities', function(){
+            if (Entities.homeRootListItems === undefined) {
+                Entities.homeRootListItems = new Entities.RootsCollection([
+                    { name: "Test", url: "test", icon: "icon-picture", navigationTrigger: "test:root", msg: "test" }
+                ]);
+            }
+            return Entities.homeRootListItems;
+        });
+        
+        
+    });
+
+    return ;
+})
