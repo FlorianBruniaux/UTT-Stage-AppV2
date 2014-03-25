2a5354e0d144804f454f7a4774e31ab613f91acf
Bruniaux <f.bruniaux@gmail.com>
2014-03-25 14:21:54 +0100

    Add items to students home page

--- app/js/entities/students/roots.js
+++ app/js/entities/students/roots.js
@@ -18,7 +18,11 @@ define([
         AppManager.reqres.setHandler('students:homeRoot:entities', function(){
             if (Entities.homeRootListItems === undefined) {
                 Entities.homeRootListItems = new Entities.RootsCollection([
-                    { name: "Test", url: "test", icon: "icon-picture", navigationTrigger: "test:root", msg: "test" }
+                    { name: "Consulter les offres", url: "offers", icon: "icon-newspaper", navigationTrigger: "students:offers:list", bg_class:"bg-success", msg: "4 nouvelles offres" },
+                    { name: "Faire une recherche", url: "research", icon: "icon-search3", navigationTrigger: "students:research:root", bg_class:" bg-info", msg: "<br />" },
+                    { name: "Gérer mon suivi", url: "monitoring", icon: "icon-list", navigationTrigger: "students:monitoring:root", bg_class:"bg-danger", msg: "1 tâche à effectuer" },
+                    { name: "Consulter les échanges", url: "messages", icon: "icon-bubble4", navigationTrigger: "students:messages:list", bg_class:"bg-success", msg: "2 nouveaux messages" },
+                    { name: "Voir mes contacts", url: "users", icon: "icon-users", navigationTrigger: "students:users:root", bg_class:" bg-info", msg: "<br />" }
                 ]);
             }
             return Entities.homeRootListItems;
