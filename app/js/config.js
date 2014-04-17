922315b2e17b3226a93844279abfe3a3145de04a
Bruniaux <f.bruniaux@gmail.com>
2014-04-17 09:58:16 +0200

    doubles quotes -> simple quote

--- app/js/config.js
+++ app/js/config.js
@@ -1,5 +1,5 @@
 // Enable/Disable DEBUG mode
-if (typeof DEBUG === "undefined") DEBUG = true;
+if (typeof DEBUG === 'undefined') DEBUG = true;
 
 // Require configuration paths etc
 requirejs.config({
@@ -8,37 +8,37 @@ requirejs.config({
         
         backbone: {
             deps: [
-                "underscore",
-                "jquery"
+                'underscore',
+                'jquery'
             ],
-            exports: "Backbone"
+            exports: 'Backbone'
         },
         
-        "backbone.picky" : ["backbone"],
+        'backbone.picky' : ['backbone'],
         
-        "backbone.syphon": ["backbone"],
+        'backbone.syphon': ['backbone'],
         
-        bootbox: ["jquery","bootstrap"],
+        bootbox: ['jquery','bootstrap'],
         
-        bootstrap: ["jquery"],
+        bootstrap: ['jquery'],
         
-        collapsible: ["jquery"],
+        collapsible: ['jquery'],
         
-        collapsibleMenu : ["collapsible"],
+        collapsibleMenu : ['collapsible'],
         
-        jasnybootstrap : ["jquery"],
+        jasnybootstrap : ['jquery'],
 
         jquery: {
-            exports: "$"
+            exports: '$'
         },
         
-        "jquery-ui": ["jquery"],
+        'jquery-ui': ['jquery'],
         
-        "jquery.validate" : ["jquery"],
+        'jquery.validate' : ['jquery'],
         
         marionette: {
-            deps: ["backbone"],
-            exports: "Marionette"
+            deps: ['backbone'],
+            exports: 'Marionette'
         },
         
         polyglot: {
@@ -46,69 +46,69 @@ requirejs.config({
         },
         
         underscore: {
-            exports: "_"
+            exports: '_'
         },
         
-        "utt.stages": {
+        'utt.stages': {
             deps: [
-                "underscore",
-                "jquery"
+                'underscore',
+                'jquery'
             ]
         },
     },
     
     paths: {
         
-        backbone: "vendors/bower/backbone/js/backbone",
+        backbone: 'vendors/bower/backbone/js/backbone',
         
         //Selectable entities as mixins for Backbone.Model and Backbone.Collection!
-        "backbone.picky": "vendors/bower/backbone.picky/js/backbone.picky",
+        'backbone.picky': 'vendors/bower/backbone.picky/js/backbone.picky',
         
-        //Serialize the forms in your Backbone.Views into a JSON object for use with Backbone"s models.
-        "backbone.syphon": "vendors/bower/backbone.syphon/js/backbone.syphon",
+        //Serialize the forms in your Backbone.Views into a JSON object for use with Backbone's models.
+        'backbone.syphon': 'vendors/bower/backbone.syphon/js/backbone.syphon',
         
         //To deal with alert boxes etc
-        bootbox: "vendors/bower/bootbox.js/js/bootbox",
+        bootbox: 'vendors/bower/bootbox.js/js/bootbox',
         
-        bootstrap: "vendors/bower/bootstrap/js/bootstrap",
+        bootstrap: 'vendors/bower/bootstrap/js/bootstrap',
         
-        collapsible: "vendors/collapsible.min",
+        collapsible: 'vendors/collapsible.min',
         
-        collapsibleMenu: "common/utilities/collapsible_menu.perso",
+        collapsibleMenu: 'common/utilities/collapsible_menu.perso',
         
         //To deak with .css files
-        css: "vendors/bower/require-css/js/css",
+        css: 'vendors/bower/require-css/js/css',
         
         //To deal with bootstrap dropdow menu
-        jasnybootstrap: "vendors/bower/jasny-bootstrap/js/jasny-bootstrap.min",
+        jasnybootstrap: 'vendors/bower/jasny-bootstrap/js/jasny-bootstrap.min',
         
-        jquery: "vendors/bower/jquery/js/jquery",
+        jquery: 'vendors/bower/jquery/js/jquery',
         
-        "jquery-ui": "vendors/bower/jquery-ui-bootstrap/js/jquery-ui-1.10.0.custom.min",
+        'jquery-ui': 'vendors/bower/jquery-ui-bootstrap/js/jquery-ui-1.10.0.custom.min',
         
-        "jquery.validate": "vendors/bower/jquery-validation/js/jquery.validate",
+        'jquery.validate': 'vendors/bower/jquery-validation/js/jquery.validate',
 
-        marionette: "vendors/bower/marionette/js/backbone.marionette",
+        marionette: 'vendors/bower/marionette/js/backbone.marionette',
        
-        polyglot:"vendors/bower/polyglot/js/polyglot",
+        polyglot:'vendors/bower/polyglot/js/polyglot',
         
         //An animated CSS3 loading spinner with VML fallback for IE.
-        spin: "vendors/loader/spin",
+        spin: 'vendors/loader/spin',
         
-        "spin.jquery": "vendors/loader/spin.jquery",
+        'spin.jquery': 'vendors/loader/spin.jquery',
         
         //To deal with .tpl files with underscore
-        tpl: "vendors/bower/requirejs-tpl/js/tpl",
+        tpl: 'vendors/bower/requirejs-tpl/js/tpl',
 
-        underscore: "vendors/bower/underscore/js/underscore",
+        underscore: 'vendors/bower/underscore/js/underscore',
         
-        "utt.stages": "vendors/utt_stages_api"
+        'utt.stages': 'vendors/utt_stages_api'
     }
 });
 
 // Initialize the application with the main application file.
 requirejs([
     
-    "main"
+    'main'
     
 ]);
