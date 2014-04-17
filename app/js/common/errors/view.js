b0153b8c570a9d724d5fb849107abdb0db195955
Bruniaux <f.bruniaux@gmail.com>
2014-04-17 14:50:58 +0200

    Add e500

--- app/js/common/errors/view.js
+++ app/js/common/errors/view.js
@@ -1,12 +1,17 @@
 define([
     'app',
-    'tpl!common/errors/templates/404.tpl'
-], function(AppManager, e404Tpl){
+    'tpl!common/errors/templates/404.tpl',
+    'tpl!common/errors/templates/500.tpl'
+], function(AppManager, e404Tpl, e500Tpl){
     
     AppManager.module('Common.Errors.View', function(View, AppManager, Backbone, Marionette, $, _){
         
         View.e404 = Marionette.ItemView.extend({
             template: e404Tpl,
+        });
+        
+        View.e500 = Marionette.ItemView.extend({
+            template: e500Tpl,
         });
 
     });
