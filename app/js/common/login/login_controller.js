73a5f5e9790244891b7abfa63506fd909a7ae720
Bruniaux <f.bruniaux@gmail.com>
2014-04-17 10:02:53 +0200

    Creation

---
+++ app/js/common/login/login_controller.js
@@ -0,0 +1,34 @@
+define([
+    'app',
+    'common/login/login_view'
+], function(AppManager, View){
+
+    // Common Root Login Controller
+    AppManager.module('Common.Login', function(Login, AppManager, Backbone, Marionette, $, _){
+        
+        Login.Controller = {
+            
+            // To list all the options of root.
+            showLogin: function(){
+                
+                if(DEBUG) console.info('common.login.showLogin()');
+
+                var view = new View.login();
+                
+                view.on('login:signOn', function(){
+                    AppManager.mainlayoutRegion.show(new View.signOn());
+                });
+                
+                view.on('login:forgotPassword', function(){
+                    AppManager.mainlayoutRegion.show(new View.forgotPassword());
+                });
+                
+                // Displays the view
+                AppManager.mainlayoutRegion.show(view);
+                
+            }  
+        }
+    });
+
+    return AppManager.Common.Login.Controller;
+});
